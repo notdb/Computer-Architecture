4cb87f529d5da382b239c9f7cf52095a9dbbba84
Doug <me@douglasbowen.com>
2019-10-14 23:46:55 -0400

    finished updating cpu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.memory = [0] * 8
+        self.pc = 0
+        self.registers = [0] * 8
 
     def load(self):
         """Load a program into memory."""
