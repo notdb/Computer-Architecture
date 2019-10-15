1b9dd87fb30632d5f6157543247fe89dec3237e0
Doug <me@douglasbowen.com>
2019-10-15 18:53:42 -0400

    add partially working print statement

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,9 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.memory = [0] * 16
+        self.ram = [0] * 255
         self.pc = 0
-        self.registers = [0] * 8
+        self.register = [0] * 8
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
@@ -37,7 +38,7 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -58,18 +59,39 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
-
+        ir = self.ram[self.pc]
+        operand_a = self.ram_read(self.pc+1)
+        print(f'{operand_a} foo')
+        operand_b = self.ram_read(self.pc+2)
+        print(f'{operand_b} bar')
+        self.trace()
+        #print(self.ram[self.pc+1])
+        #print(self.ram[self.pc+2])
+        while self.running:
+            command = self.ram[self.pc]
+            print(command)
+            #LDI - set value of register to integer
+            if command == 130:
+                self.register[operand_a] = operand_b
+                print(self.pc)
+                print(ir)
+                print(f'{self.ram} NAME')
+                self.pc+1
+            #HLT - stops the program
+            elif command == 1:
+                self.running = False
+                self.pc += 1
+            #PRN - prints next digit in register
+            elif command == 71:
+                num = print(self.ram[self.pc+1])
+                self.pc += 1
     def ram_read(self, MAR):
-        if MAR not in self.memory:
-            return print('MAR not found')
-        return self.memory[MAR]
+        return self.ram[MAR]
 
     def ram_write(self, MAR, MDR):
-        return self.memory[MAR] == MDR
+        self.ram[MAR] = MDR
