728b42e3863472a206c8578af387c3aefcf92a7d
Doug <me@douglasbowen.com>
2019-10-16 16:25:52 -0400

    add load_management function to print print8.ls8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,7 @@ class CPU:
         self.pc = 0
         self.register = [0] * 8
         self.running = True
+        self.garbage = []
 
     def load(self):
         """Load a program into memory."""
@@ -33,7 +34,38 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
+    def load_management(self, filename):
+        try:
+            address = 0
 
+            with open(filename) as f:
+                for line in f:
+                    # ignore comments
+                    comment_split = line.split("#")
+
+                    # Convert from binary to int
+                    num = comment_split[0].strip()
+                    
+                    #try:
+                        
+                    print(comment_split)
+                    if len(num) == 8:
+                        print(f'{type(num)}: {len(num)} {num} FOOBAR')
+                        val = int(num, 2)
+                        print(f'{val} TEST TEST')
+                        self.ram[address] = val
+                        address += 1
+                    else:
+                        self.garbage.append(num)
+                    #except ValueError:
+                        #continue
+                    
+                    #print(f"{val:08b}: {val:d}: {val}")
+            print(self.garbage)
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
+            
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -72,6 +104,7 @@ class CPU:
         self.trace()
         #print(self.ram[self.pc+1])
         #print(self.ram[self.pc+2])
+        print(self.ram)
         while self.running:
             command = self.ram[self.pc]
             #print(f'{command} this is the command')
